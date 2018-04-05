84ac13b22767db2ced45ce4d9e7248aba9a7f31d
ravic1995 <ravi.chandak901@gmail.com>
2018-04-05 16:18:32 +0800

    Return lsit of image and label

--- svm/tf_file_reader.py
+++ svm/tf_file_reader.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import os
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from PIL import  Image
 
 # data_path = 'train.tfrecords'  # address to save the hdf5 file
@@ -30,29 +30,20 @@ def run():
         image = tf.decode_raw(features['image'], tf.uint8)
         image = tf.cast(image, tf.float32)
 
-        # image = tf.reshape(image, tf.stack([720,720,3]))
-        # image.set_shape([720, 720, 3])
-
         # Cast label data into int32
         label = tf.cast(features['label'], tf.int32)
         # Reshape image data into the original shape
         init_op = [tf.global_variables_initializer(), tf.local_variables_initializer()]
         sess.run(init_op)
-        print("Whats happ")
-        # example, l = sess.run([image, label])
-        # img = Image.fromarray(example, 'RGB')
+
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
+        train_list = []
         for i in range(500):
             example, l = sess.run([image, label])
-            # img = Image.fromarray(example, 'RGB')
-            # img.save(DATASET_PATH + 'filename-train.jpeg')
-
-            print (example, l)
+            train_list.append((example,l))
+            # print (example, l)
         coord.request_stop()
         coord.join(threads)
-
-        print("saved")
-        print (example, l)
-
+        return  train_list
 run()
