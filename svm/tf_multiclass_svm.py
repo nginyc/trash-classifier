d0a5e75402d7c66a5a90adf1a5f415be56e6eb24
ravic1995 <ravi.chandak901@gmail.com>
2018-04-07 01:48:59 +0800

    Add feature selection using k-means, comment SVM part of code

--- svm/tf_multiclass_svm.py
+++ svm/tf_multiclass_svm.py
@@ -26,6 +26,7 @@ from sklearn import datasets
 from tensorflow.python.framework import ops
 from common import tfrecords_to_dataset
 import tf_file_reader
+import scipy.cluster.vq as vq
 
 tf.logging.set_verbosity(tf.logging.DEBUG)
 
@@ -34,7 +35,7 @@ SVM_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 ops.reset_default_graph()
 
 # Create graph
-sess = tf.Session()
+# sess = tf.Session()
 # reads the data_set as of now just to check if this is running
 # only reads around 1000 images
 # NOTE :  PLEASE USE MY VERSION OF TF generator and extractor as of now will change it correctly later SORRY!!
@@ -53,6 +54,7 @@ with tf.Session() as sess:
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
     feature = []
+    onlydes = []
     for data in (dataset):
         # need to transform data from a 1D array to an acceptable form by CV2
         img = data[0][0:512 * 384].reshape(384, 512)
@@ -62,11 +64,37 @@ with tf.Session() as sess:
         kp = orb.detect(img,None)
         kp, des = orb.compute(img, kp)
         feature.append((des, data[1]))
+        onlydes.append(des)
     print(feature[0][0].shape)
     coord.request_stop()
     coord.join(threads)
     sess.close()
+print(onlydes[0].shape[0])
+nkeys = len(onlydes)
+print(nkeys)
 
+array = np.zeros((nkeys * 1000, 32))
+pivot = 0
+for key in range(len(onlydes)):
+    value = onlydes[key]
+    # print(value)
+    nelements = value.shape[0]
+    while pivot + nelements > array.shape[0]:
+        padding = np.zeros_like(array)
+        array = np.vstack((array, padding))
+    array[pivot:pivot + nelements] = value
+    pivot += nelements
+array = np.resize(array, (pivot, 32))
+
+nfeatures = array.shape[0]
+nclusters = int(np.sqrt(nfeatures))
+print nclusters
+codebook, distortion = vq.kmeans(array,
+                                 nclusters,
+                                 thresh=1)
+print "CodeBook"
+print codebook
+# onlydes
 # TODO : Follwo the example commented out in this part
 # Implement SVM using the code below to best fit out need
 
@@ -85,10 +113,21 @@ with tf.Session() as sess:
 # iris.data = [(Sepal Length, Sepal Width, Petal Length, Petal Width)]
 # iris = datasets.load_iris()
 
+
+# these are the featues
 # x_vals = np.array([[x[0], x[3]] for x in iris.data])
+# classes correspond to which being true
 # y_vals1 = np.array([1 if y == 0 else -1 for y in iris.target])
 # y_vals2 = np.array([1 if y == 1 else -1 for y in iris.target])
 # y_vals3 = np.array([1 if y == 2 else -1 for y in iris.target])
+
+# forms a 2d array
+#[[0, 0, 1],
+#[0, 1, 0],
+#[1, 0, 0]]
+
+# feature vertex for each class
+# each class has class1_X = feature 1 value
 # y_vals = np.array([y_vals1, y_vals2, y_vals3])
 # class1_x = [x[0] for i, x in enumerate(x_vals) if iris.target[i] == 0]
 # class1_y = [x[1] for i, x in enumerate(x_vals) if iris.target[i] == 0]
@@ -97,26 +136,25 @@ with tf.Session() as sess:
 # class3_x = [x[0] for i, x in enumerate(x_vals) if iris.target[i] == 2]
 # class3_y = [x[1] for i, x in enumerate(x_vals) if iris.target[i] == 2]
 #
-# # Declare batch size
+# Declare batch size
 # batch_size = 50
-#
+# #
 # # Initialize placeholders
 # x_data = tf.placeholder(shape=[None, 2], dtype=tf.float32)
 # y_target = tf.placeholder(shape=[3, None], dtype=tf.float32)
 # prediction_grid = tf.placeholder(shape=[None, 2], dtype=tf.float32)
-#
-# # Create variables for svm
+# #
+# # # Create variables for svm
 # b = tf.Variable(tf.random_normal(shape=[3, batch_size]))
 #
-# # Gaussian (RBF) kernel
+#  # Gaussian (RBF) kernel
 # gamma = tf.constant(-10.0)
 # dist = tf.reduce_sum(tf.square(x_data), 1)
-# dist = tf.reshape(dist, [-1, 1])
-# sq_dists = tf.multiply(2., tf.matmul(x_data, tf.transpose(x_data)))
+# dist = tf.reshape(dist, [-1, 1]) # basically say that transform the tensor to [[one value], [1v] ,[1v]]
+# sq_dists = tf.multiply(2., tf.matmul(x_data, tf.transpose(x_data))) # x,2 2,x == x,x
 # my_kernel = tf.exp(tf.multiply(gamma, tf.abs(sq_dists)))
 #
-#
-# # Declare function to do reshape/batch multiplication
+#  # Declare function to do reshape/batch multiplication
 # def reshape_matmul(mat):
 #     v1 = tf.expand_dims(mat, 1)
 #     v2 = tf.reshape(v1, [3, batch_size, 1])
@@ -141,22 +179,22 @@ with tf.Session() as sess:
 # prediction_output = tf.matmul(tf.multiply(y_target, b), pred_kernel)
 # prediction = tf.arg_max(prediction_output - tf.expand_dims(tf.reduce_mean(prediction_output, 1), 1), 0)
 # accuracy = tf.reduce_mean(tf.cast(tf.equal(prediction, tf.argmax(y_target, 0)), tf.float32))
-#
-# # Declare optimizer
+
+# Declare optimizer
 # my_opt = tf.train.GradientDescentOptimizer(0.01)
 # train_step = my_opt.minimize(loss)
-#
-# # Initialize variables
+
+# Initialize variables
 # init = tf.global_variables_initializer()
 # sess.run(init)
-#
-# # Training loop
+
+# Training loop
 # loss_vec = []
 # batch_accuracy = []
 # for i in range(100):
-#     rand_index = np.random.choice(len(x_vals), size=batch_size)
-#     rand_x = x_vals[rand_index]
-#     rand_y = y_vals[:, rand_index]
+#     rand_index = np.random.choice(len(x_vals), size=batch_size) # randomly choose batch size of the examples
+#     rand_x = x_vals[rand_index] # random rows of weights
+#     rand_y = y_vals[:, rand_index] # random colmns getting assignment
 #     sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
 #
 #     temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
@@ -182,7 +220,11 @@ with tf.Session() as sess:
 #                                                    prediction_grid: grid_points})
 # grid_predictions = grid_predictions.reshape(xx.shape)
 #
+
+
 # # Plot points and grid
+
+
 # plt.contourf(xx, yy, grid_predictions, cmap=plt.cm.Paired, alpha=0.8)
 # plt.plot(class1_x, class1_y, 'ro', label='I. setosa')
 # plt.plot(class2_x, class2_y, 'kx', label='I. versicolor')
