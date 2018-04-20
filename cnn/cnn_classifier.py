18568351d56c7353f83e31022c8ecc5584dffe84
Charlton Lim <cadmusthefounder@gmail.com>
2018-04-20 15:53:15 +0800

    inception

--- cnn/cnn_classifier.py
+++ cnn/cnn_classifier.py
@@ -10,63 +10,66 @@ import shutil
 tf.logging.set_verbosity(tf.logging.INFO)
 
 alexnet_params = {
-    'batch_size': 16,
-    'learning_rate': 0.002,
-    'train_steps': 400,
-    'eval_steps': 10,
-    'num_classes': 5,
-    'image_height': 256,
-    'image_width': 256,
-    'image_channels': 3,
-    'architecture': alexnet_architecture,
-    'save_checkpoints_steps': 100,
-    'use_checkpoint': True,
-    'log_step_count_steps': 1,
-    'logging_steps': 5,
-    'tf_random_seed': 20170409,
-    'model_name': 'alexnet_model'
+	'batch_size': 16,
+	'learning_rate': 0.002,
+	'train_steps': 1000,
+	'eval_steps': 1,
+	'eval_batch_size': 717,
+	'num_classes': 5,
+	'image_height': 256,
+	'image_width': 256,
+	'image_channels': 3,
+	'architecture': alexnet_architecture,
+	'save_checkpoints_steps': 100,
+	'use_checkpoint': False,
+	'log_step_count_steps': 1,
+	'logging_steps': 5,
+	'tf_random_seed': 20170409,
+	'model_name': 'alexnet_model'
 }
 
 zfnet_params = {
-    'batch_size': 16,
-    'learning_rate': 0.002,
-    'train_steps': 1000,
-    'eval_steps': 10,
-    'num_classes': 5,
-    'image_height': 256,
-    'image_width': 256,
-    'image_channels': 3,
-    'architecture': zfnet_architecture,
-    'save_checkpoints_steps': 100,
-    'use_checkpoint': False,
-    'log_step_count_steps': 1,
-    'logging_steps': 10,
-    'tf_random_seed': 20170417,
-    'model_name': 'zfnet_model'
+	'batch_size': 16,
+	'learning_rate': 0.002,
+	'train_steps': 1000,
+	'eval_steps': 1,
+	'eval_batch_size': 717,
+	'num_classes': 5,
+	'image_height': 256,
+	'image_width': 256,
+	'image_channels': 3,
+	'architecture': zfnet_architecture,
+	'save_checkpoints_steps': 100,
+	'use_checkpoint': False,
+	'log_step_count_steps': 1,
+	'logging_steps': 10,
+	'tf_random_seed': 20170417,
+	'model_name': 'zfnet_model'
 }
 
 inception_params = {
-    'batch_size': 16,
-    'learning_rate': 0.002,
-    'train_steps': 1000,
-    'eval_steps': 10,
-    'num_classes': 5,
-    'image_height': 299,
-    'image_width': 299,
-    'image_channels': 3,
-    'architecture': inception_architecture,
-    'save_checkpoints_steps': 100,
-    'use_checkpoint': False,
-    'log_step_count_steps': 1,
-    'logging_steps': 10,
-    'tf_random_seed': 20170418,
-    'model_name': 'inception_model'
+	'batch_size': 16,
+	'learning_rate': 0.002,
+	'train_steps': 1000,
+	'eval_steps': 1,
+	'eval_batch_size': 717,
+	'num_classes': 5,
+	'image_height': 299,
+	'image_width': 299,
+	'image_channels': 3,
+	'architecture': inception_architecture,
+	'save_checkpoints_steps': 100,
+	'use_checkpoint': False,
+	'log_step_count_steps': 1,
+	'logging_steps': 10,
+	'tf_random_seed': 20170418,
+	'model_name': 'inception_model'
 }
 
 architecture = {
-    'alexnet': alexnet_params,
-    'zfnet': zfnet_params,
-    'inception': inception_params
+	'alexnet': alexnet_params,
+	'zfnet': zfnet_params,
+	'inception': inception_params
 }
 
 def cnn_model_fn(features, labels, mode, params):
@@ -96,55 +99,55 @@ def cnn_model_fn(features, labels, mode, params):
 	return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 def eval_confusion_matrix(labels, predictions, params):
-    with tf.variable_scope("eval_confusion_matrix"):
-        con_matrix = tf.confusion_matrix(labels=labels, predictions=predictions, num_classes=params['num_classes'])
-        con_matrix_sum = tf.Variable(
-            tf.zeros(
-                shape=(params['num_classes'], params['num_classes']), 
-                dtype=tf.int32
-            ),
-            trainable=False,
-            name="confusion_matrix_result",
-            collections=[tf.GraphKeys.LOCAL_VARIABLES]
-        )
-        update_op = tf.assign_add(con_matrix_sum, con_matrix)
-        return tf.convert_to_tensor(con_matrix_sum), update_op
+	with tf.variable_scope("eval_confusion_matrix"):
+		con_matrix = tf.confusion_matrix(labels=labels, predictions=predictions, num_classes=params['num_classes'])
+		con_matrix_sum = tf.Variable(
+			tf.zeros(
+				shape=(params['num_classes'], params['num_classes']), 
+				dtype=tf.int32
+			),
+			trainable=False,
+			name="confusion_matrix_result",
+			collections=[tf.GraphKeys.LOCAL_VARIABLES]
+		)
+		update_op = tf.assign_add(con_matrix_sum, con_matrix)
+		return tf.convert_to_tensor(con_matrix_sum), update_op
 
 def main(argv):
-    try:
-        opts, args = getopt.getopt(argv[1:], "ha:", ["help=", "architecture="])
-        for opt, arg in opts:
-            if opt in ("-h", "--help"):
-                print("{} -a <architecture>".format(argv[0]))
-                sys.exit()
-            elif opt in ("-a", "--architecture"):
-                params = architecture[arg]
-    except getopt.GetoptError:
-        print("{} -a <architecture>".format(argv[0]))
-        sys.exit()
+	try:
+		opts, args = getopt.getopt(argv[1:], "ha:", ["help=", "architecture="])
+		for opt, arg in opts:
+			if opt in ("-h", "--help"):
+				print("{} -a <architecture>".format(argv[0]))
+				sys.exit()
+			elif opt in ("-a", "--architecture"):
+				params = architecture[arg]
+	except getopt.GetoptError:
+		print("{} -a <architecture>".format(argv[0]))
+		sys.exit()
 
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    model_directory =  os.path.join(current_directory, "..", "model", params['model_name'])
-    train_data_files = [os.path.join(current_directory, "..", "data", "tfrecords", "train.tfrecords")]
-    test_data_files = [os.path.join(current_directory, "..", "data", "tfrecords", "test.tfrecords")]
-    
-    if not params['use_checkpoint']:
-        print("Removing previous artifacts...")
-        shutil.rmtree(model_directory, ignore_errors=True)
+	current_directory = os.path.dirname(os.path.abspath(__file__))
+	model_directory =  os.path.join(current_directory, "..", "model", params['model_name'])
+	train_data_files = [os.path.join(current_directory, "..", "data", "tfrecords", "train.tfrecords")]
+	test_data_files = [os.path.join(current_directory, "..", "data", "tfrecords", "test.tfrecords")]
+	
+	if not params['use_checkpoint']:
+		print("Removing previous artifacts...")
+		shutil.rmtree(model_directory, ignore_errors=True)
 
-    estimator = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir=model_directory, params=params)
+	estimator = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir=model_directory, params=params)
 
-    tensors_to_log = {"probabilities": "softmax_tensor"}
-    logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=params['logging_steps'])
+	tensors_to_log = {"probabilities": "softmax_tensor"}
+	logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=params['logging_steps'])
 
-    train_input_fn = lambda: input_fn(train_data_files, params)
-    estimator.train(train_input_fn, steps=params['train_steps'], hooks=[logging_hook])
-    
-    test_input_fn = lambda: input_fn(test_data_files, params)
-    eval_results = estimator.evaluate(test_input_fn, steps=params['eval_steps'], hooks=[logging_hook])
+	train_input_fn = lambda: input_fn(train_data_files, params, mode=tf.estimator.ModeKeys.TRAIN)
+	estimator.train(train_input_fn, steps=params['train_steps'], hooks=[logging_hook])
+	
+	test_input_fn = lambda: input_fn(test_data_files, params, mode=tf.estimator.ModeKeys.EVAL)
+	eval_results = estimator.evaluate(test_input_fn, steps=params['eval_steps'], hooks=[logging_hook])
 
-    print("Accuracy: {}".format(eval_results['accuracy']))
-    print("Confusion Matrix: \n{}".format(eval_results['confusion_matrix']))
+	print("Accuracy: {}".format(eval_results['accuracy']))
+	print("Confusion Matrix: \n{}".format(eval_results['confusion_matrix']))
 
 if __name__ == "__main__":
-    tf.app.run()
+	tf.app.run()
